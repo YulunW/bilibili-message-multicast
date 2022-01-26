f943ff62c282681261df94470f5aba754640f788
Yulun Wang <yulunwan@usc.edu>
2022-01-26 00:12:17 -0800

    helper: create constants

---
+++ src/helpers/constants.ts
@@ -0,0 +1,6 @@
+export const USER_AGENT =
+  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit / 537.36(KHTML, like Gecko) Chrome / 61.0.3163.100 Safari / 537.36';
+export const MAIN_SESSION_STRING = 'persist:MAIN_SESSION';
+
+// DUMMY import to keep the linter happy
+export const DUMMY = 10;
